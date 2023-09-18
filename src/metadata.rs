77832f0c335276d9300fd8e130db2379ef50e583
kaoru <k@warpnine.io>
2023-09-18 18:25:36 +0900

    refactor: more generic implementation

--- src/metadata.rs
+++ src/metadata.rs
@@ -14,48 +14,44 @@ pub struct Metadata {
     pub ip_address: Option<IpAddr>,
 
     /// City of the client.
-    pub city: Option<String>,
+    pub city: String,
 
     /// Country of the client.
-    pub country: Option<String>,
+    pub country: String,
 
     /// ASN of the client.
     pub asn: String,
 
     /// Timezone of the client.
-    pub timezone: Option<String>,
+    pub timezone: String,
 
     /// Request time of the client.
-    pub request_time: Option<i64>,
+    pub request_time: i64,
 }
 
 impl TryFrom<&HeaderMap> for Metadata {
     type Error = Box<dyn Error>;
 
     fn try_from(headers: &HeaderMap) -> Result<Self, Self::Error> {
-        let latitude = parse_header::<f64>(headers, "latitude")?;
-        let longitude = parse_header::<f64>(headers, "longitude")?;
-
-        let ip_address = parse_header::<String>(headers, "ip")?.map_or_else(
-            || Ok(None),
-            |ip| {
-                ip.parse::<IpAddr>()
-                    .map(Some)
-                    .map_err(|e| Box::new(e) as Box<dyn Error>)
-            },
-        )?;
-
-        let city = parse_header::<String>(headers, "city")?;
-        let country = parse_header::<String>(headers, "country")?;
-        let asn = parse_header::<String>(headers, "asn").map_or_else(
-            |_| "".to_string(),
-            |val| val.map_or_else(|| "".to_string(), |asn| format!("AS{}", asn)),
-        );
-        let timezone = parse_header::<String>(headers, "timezone")?;
-        let request_time = parse_header::<i64>(headers, "request-time")?;
+        let coordinate = (
+            get_header_value::<f64>(headers, "latitude")?,
+            get_header_value::<f64>(headers, "longitude")?,
+        )
+            .into();
+        let ip_address =
+            get_header_value_and_process::<String, Option<IpAddr>, _>(headers, "ip", |ip| {
+                ip.parse::<IpAddr>().ok()
+            })?;
+        let city = get_header_value::<String>(headers, "city")?;
+        let country = get_header_value::<String>(headers, "country")?;
+        let asn = get_header_value_and_process::<String, String, _>(headers, "asn", |asn| {
+            format!("AS{}", asn)
+        })?;
+        let timezone = get_header_value::<String>(headers, "timezone")?;
+        let request_time = get_header_value::<i64>(headers, "request-time")?;
 
         Ok(Self {
-            coordinate: (latitude, longitude).into(),
+            coordinate,
             ip_address,
             city,
             country,
@@ -66,22 +62,45 @@ impl TryFrom<&HeaderMap> for Metadata {
     }
 }
 
-/// Parse a header value into a type.
+/// Get a header value.
 ///
 /// # Arguments
 ///
-/// * `headers` - The headers to parse.
-/// * `name` - The name of the header to parse. Prefix `cf-meta-` will be added.
+/// - `headers` - The headers to parse.
+/// - `name` - The name of the header to parse.
 ///
 /// # Returns
 ///
-/// * `Ok(Some(T))` - The parsed value.
-/// * `Ok(None)` - The header was not present.
-/// * `Err(e)` - The header was present but could not be parsed.
-fn parse_header<T>(headers: &HeaderMap, name: &'static str) -> Result<Option<T>, Box<dyn Error>>
+/// The parsed value.
+fn get_header_value<'a, T>(headers: &HeaderMap, name: &'a str) -> Result<T, Box<dyn Error>>
+where
+    T: FromStr + Default,
+    <T as FromStr>::Err: Error + 'static,
+{
+    get_header_value_and_process(headers, name, |x: T| x.into())
+}
+
+/// Get a header value and process it with given function.
+///
+/// # Arguments
+///
+/// - `headers` - The headers to parse.
+/// - `name` - The name of the header to parse.
+/// - `processor` - The function to process the parsed value.
+///
+/// # Returns
+///
+/// The parsed value.
+fn get_header_value_and_process<'a, T, U, F>(
+    headers: &HeaderMap,
+    name: &'a str,
+    processor: F,
+) -> Result<U, Box<dyn Error>>
 where
     T: FromStr,
     <T as FromStr>::Err: Error + 'static,
+    U: Default,
+    F: Fn(T) -> U,
 {
     headers
         .get(format!("cf-meta-{name}"))
@@ -90,6 +109,7 @@ where
                 .map_err(|e| Box::new(e) as Box<dyn Error>)?
                 .parse::<T>()
                 .map_err(|e| Box::new(e) as Box<dyn Error>)
+                .map(|s| processor(s))
         })
-        .transpose()
+        .unwrap_or(Ok(U::default()))
 }
