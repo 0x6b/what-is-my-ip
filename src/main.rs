d6a7ec1b46fea34cf5b99680ba53369efd08ffb2
kaoru <k@warpnine.io>
2024-12-02 22:23:02 +0900

    feat: display only IP address by default

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+use std::net::IpAddr;
+
 use anyhow::Result;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(version)]
+struct Args {
+    /// Display detailed information.
+    #[arg(short, long)]
+    verbose: bool,
+}
+
 fn main() -> Result<()> {
-    println!("{}", what_is_my_ip::Client::get()?);
+    let Args { verbose } = Args::parse();
+    let metadata = what_is_my_ip::Client::get()?;
+
+    if verbose {
+        println!("{metadata}");
+    } else {
+        println!("{}", metadata.ip_address.unwrap_or(IpAddr::from([0, 0, 0, 0])));
+    }
 
     Ok(())
 }
